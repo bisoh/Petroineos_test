30a7fdafe0c82d2279638d13c2e9ca90d7de60fe
Bassem Murad <bassem.murad@gmail.com>
2022-10-19 22:44:39 +0100

    adding missed exporter

---
+++ PetroineosService/Services/CsvExporter.cs
@@ -0,0 +1,11 @@
+﻿namespace PetroineosService.Services;
+
+public class CsvExporter : IExporter
+{
+    private const string Extension = ".csv";
+    
+    public async Task Export(string fileName, string content)
+    {
+        await File.WriteAllTextAsync($"{fileName}.{Extension}", content);
+    }
+}
